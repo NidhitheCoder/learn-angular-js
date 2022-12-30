75d9ac94f277923a5a46677cb1a6214ca61a04f0
Nidheesh PV <nidheesh@vonnue.com>
2022-12-30 23:19:47 +0530

    feat: modules in angular js

---
+++ app/app.js
@@ -0,0 +1,14 @@
+const myNinjaApp = angular.module('myNinjaApp', []);
+
+// Before application run
+myNinjaApp.config(() => {
+
+});
+
+// Fire when the application run
+myNinjaApp.run(() => {
+
+});
+
+
+myNinjaApp.controller;
