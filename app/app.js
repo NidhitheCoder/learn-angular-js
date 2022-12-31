b8c710c507f27a948e8dc7bf6af2a03d05094c5a
Nidheesh PV <nidheesh@vonnue.com>
2022-12-31 17:07:58 +0530

    feat: controller in angular js

--- app/app.js
+++ app/app.js
@@ -1,14 +1,20 @@
-const myNinjaApp = angular.module('myNinjaApp', []);
+const myNinjaApp = angular.module("myNinjaApp", []);
 
 // Before application run
-myNinjaApp.config(() => {
-
-});
+myNinjaApp.config(() => {});
 
 // Fire when the application run
-myNinjaApp.run(() => {
+myNinjaApp.run(() => {});
 
+// function inside controller contain code for 'NinjaController' section of the web application
+myNinjaApp.controller("NinjaController", ($scope) => {
+  $scope.message = "message from controller";
+  $scope.ninjas = ["Tom", "Jenny", "jerry", "David", "smith"];
 });
 
 
-myNinjaApp.controller;
+// For protect from minification
+myNinjaApp.controller("NinjaController", ['$scope', ($scope) => {
+    $scope.message = "message from controller";
+    $scope.ninjas = ["Tom", "Jenny", "jerry", "David", "smith"];
+  }]);
